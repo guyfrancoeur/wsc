374d86a6df15297d7ac8268cd9e94a2ff2c299f0
guyfrancoeur <guyfrancoeur@users.noreply.github.com>
2020-09-14 09:15:07 -0400

    v2.3

--- index.js
+++ index.js
@@ -129,6 +129,7 @@ function init() {
         $('#messages').append($('<li>').html(text));
         elem.scrollTop = elem.scrollHeight;
         break;
+      case 'cln' : window.location.reload(true); break;
       }
     }
   }
