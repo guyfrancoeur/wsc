7c02d08f7f5c3e2e7b27054de9251a1fe7fffab3
Marie287 <marie.bruggeman287@gmail.com>
2020-09-17 19:59:29 +0200

    correction

--- index.js
+++ index.js
@@ -166,7 +166,7 @@ $(window).keyup(function(e){
 $(document).ready(function(){
   //$('[data-toggle="tooltip"]').tooltip();
   $('#m_aye').load('./m.aye.html');
-  //if (devtools.isOpen) $('#m_aye').modal({backdrop: 'static', keyboard: false});
+  if (devtools.isOpen) $('#m_aye').modal({backdrop: 'static', keyboard: false});
   $('#name').focus();
   $('#welcome').hide();
   $('[data-toggle="popover"]').popover({html: true});
