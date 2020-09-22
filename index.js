1c7c81df5a9a588cf441fbf9d93d75b14995748d
guyfrancoeur <guyfrancoeur@users.noreply.github.com>
2020-09-22 10:47:48 -0400

    v2.6.1

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var a1 = new Audio('./consequence.mp3');
 var a2 = new Audio('./inbox.mp3');
 var connecte = 0;
+var start = new Date().getTime();
 var ws = new WebSocket("wss://echo.websocket.org");
-ws.onopen = function() { ws.close(); console.log('echo succès'); };
+ws.onopen = function(uri) { console.log("onopen of", uri, "in", (new Date().getTime() - start), "ms"); ws.close(); };
 var elem = document.getElementById('messages'); //pour scroll-auto
 
 const ico = document.getElementById('favicon');
@@ -131,7 +132,8 @@ function init() {
   console.log("A", ws.readyState);
   console.log("B", ws.url);
   console.log("C", ws.readyState);
-  ws.onopen = function() {
+  ws.onopen = function(uri) {
+    console.log("onopen of", uri, "in", (new Date().getTime() - start), "ms");
     ws.send(JSON.stringify({
       type: 'link',
       name: '',
@@ -209,9 +211,9 @@ $(document).ready(function(){
     $('#name').val($.trim($('#name').val()));
     if ($('#name').val().length > 0 && $('#room').val().length > 0 && $('#motsecret').val().length > 0) {
       //$("#loading").show();
-      console.log("readyState :", ws.readyState);
-      ws = new WebSocket("wss://www.salutem.co:"+ $('#room').val() +"/");
       init();
+      start = new Date().getTime();
+      ws = new WebSocket("wss://www.salutem.co:"+ $('#room').val() +"/");
     }
   });
 
