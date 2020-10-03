46a02a4d4b27f7c793c050e017e0b5cf5743ef05
guyfrancoeur <guyfrancoeur@users.noreply.github.com>
2020-10-03 17:01:04 -0400

    v3.0b1

--- js/m.i.js
+++ js/m.i.js
@@ -2,71 +2,53 @@ var CLIPBOARD = new CLIPBOARD_CLASS("#preview", true);
 
 /**
  * image pasting into canvas virtual canvas then <img>.
- * 
+ *
  * @param {string} elem - canvas id
  * @param {boolean} autoresize - if canvas will be resized (not used)
  */
 function CLIPBOARD_CLASS(elem, autoresize) {
-	var _self = this;
-  //create canvas ici
+  var _self = this;
   var canvas = document.createElement('canvas');
-  var ctx = canvas.getContext('2d');
-  var img = $(elem)
-  
-	//var canvas = document.getElementById(elem);
-	//var ctx = document.getElementById(elem).getContext("2d");
 
-	//handlers
-	document.addEventListener('paste', function (e) { _self.paste_auto(e); }, false);
+  document.addEventListener('paste', function (e) { _self.paste_auto(e); }, false);
 
-	//on paste
-	this.paste_auto = function (e) {
-		if (e.clipboardData) {
-			var items = e.clipboardData.items;
-			if (!items) return;
-			
-			var is_image = false;
-			for (var i = 0; i < items.length; i++) {
-				if (items[i].type.indexOf("image") !== -1) {
-					//image
-					var blob = items[i].getAsFile();
-          
+  this.paste_auto = function (e) {
+    if (e.clipboardData) {
+      var items = e.clipboardData.items;
+      if (!items) return;
+
+      var is_image = false;
+      for (var i = 0; i < items.length; i++) {
+        if (items[i].type.indexOf("image") !== -1) {
+          var blob = items[i].getAsFile();
           var URLObj = window.URL || window.webkitURL;
-					var url = URLObj.createObjectURL(blob);
-          img.src = url;
-          
-          ctx.drawImage(img, 0, 0);
-          uri = canvas.toDataURL('image/jpeg', 1.0);
-          $(elem).attr('src', uri); // GF
-					//this.paste_createImage(source);
-					is_image = true;
-				}
-			}
-			if(is_image == true){
-				e.preventDefault();
-			}
-		}
-	};
-  
-	//draw pasted image to canvas
-  /*
-	this.paste_createImage = function (source) {
-		var pastedImage = new Image();
-		pastedImage.onload = function () {
-			if(autoresize == true){
-				//resize
-				canvas.width = pastedImage.width;
-				canvas.height = pastedImage.height;
-			}
-			else{
-				//clear canvas
-				ctx.clearRect(0, 0, canvas.width, canvas.height);
-			}
-			ctx.drawImage(pastedImage, 0, 0);
-		};
-		pastedImage.src = source;
-	};
-  */
+          var url = URLObj.createObjectURL(blob);
+          this.paste_createImage(url);
+          is_image = true;
+        }
+      }
+      if(is_image == true){
+        e.preventDefault();
+      }
+    }
+  };
+
+  this.paste_createImage = function (source) {
+    var pastedImage = new Image();
+    pastedImage.onload = function () {
+      if(autoresize == true){
+        canvas.width = pastedImage.width;
+        canvas.height = pastedImage.height;
+      } else {
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height); //clear canvas
+      }
+      canvas.getContext('2d').drawImage(pastedImage, 0, 0);
+      uri = canvas.toDataURL('image/jpeg', 1.0);
+      $(elem).attr('src', uri);
+    };
+    pastedImage.src = source;
+  };
+
 }
 
 $('#bmSubmit').on('click', function(){
