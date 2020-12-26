188115aee2b5abedd332cd526e2f4d581e19487b
guyfrancoeur <guyfrancoeur@users.noreply.github.com>
2020-12-26 18:07:21 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -167,7 +167,7 @@ function init() {
     if (evt.data != "") {
       data = JSON.parse(evt.data);
       switch (data.type) {
-        case 'lnk' : $('#count').text(data.count); $('#users').empty(); $('#users').append(data.message); /*$('#name').text(data.name);*/ $('#welcometext').text('Bonjour ' + $('#name').val()); resizeModaleUsagers(); break;
+        case 'lnk' : $('#count').text(data.count); $('#users').empty(); $('#users').append(data.message); /*$('#name').text(data.name);*/ $('#welcometext').text('Bonjour'); resizeModaleUsagers(); break;
         case 'tlk' :
           if (!document.hasFocus()) { newUpdate(); a2.play(); }
           var text = data.message;
