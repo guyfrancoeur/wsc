16edcced3a1fd86f87818717fd715644bf60a1c3
guyfrancoeur <guyfrancoeur@users.noreply.github.com>
2021-01-03 14:45:48 -0500

    Update m.i.js

--- js/m.i.js
+++ js/m.i.js
@@ -61,13 +61,13 @@ $('#bmSubmit').on('click', function(){
 });
 
 //ceci resize modal-content
-$('#m_i_mc').resizable({ 
+$('#m_i').resizable({ 
   alsoResize: ".modal-dialog",
   minHeight: 200,
   minWidth: 300,
   maxWidth: 450
 });
-$('#m_i_md').draggable(); //modal-dialog
+$('#m_i').draggable(); //modal-dialog
 
 //ceci est-ce necessaire ?
 //$('#m_i').on('show.bs.modal', function() {
